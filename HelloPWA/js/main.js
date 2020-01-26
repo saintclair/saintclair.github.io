85822e2aaa22acf1309f9d8c17b42dc53cc6fb76
Saint Clair <saintclair.com@gmail.com>
2020-01-26 12:09:14 -0300

    test pwa

--- HelloPWA/js/main.js
+++ HelloPWA/js/main.js
@@ -2,6 +2,18 @@ window.onload = () => {
   'use strict';
 
   if ('serviceWorker' in navigator) {
-    navigator.serviceWorker.register('sw.js');
+    navigator.serviceWorker
+    	.register('./sw.js')        
+    	.then(function(registration) {
+          // SUCESSO - ServiceWorker Registrado
+          console.log(
+            "ServiceWorker registrado com sucesso no escopo: ",
+            registration.scope
+          );
+        })
+        .catch(function(err) {
+          // ERRO - Falha ao registrar o ServiceWorker
+          console.log("Falha ao registrar o ServiceWorker: ", err);
+        });;
   }
 }
