9decb4c831461b236e6859698b5deae21539e8e2
Saint Clair <saintclair.com@gmail.com>
2020-01-26 11:36:00 -0300

    fixed path

--- HelloPWA/sw.js
+++ HelloPWA/sw.js
@@ -1,9 +1,9 @@
 var cacheName = 'hello-pwa';
 var filesToCache = [
-  '/',
-  '/index.html',
-  '/css/style.css',
-  '/js/main.js'
+  '/HelloPWA/',
+  '/HelloPWA/index.html',
+  '/HelloPWA/css/style.css',
+  '/HelloPWA/js/main.js'
 ];
 
 /* Start the service worker and cache all of the app's content */
